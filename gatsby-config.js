03228330b611a09ab144e61463d4194977b47300
akdev5 <akio.mori555@gmail.com>
2020-03-18 10:56:15 +0200

    update layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `The Gray Web Blog`,
+    title: 'The Web Blog',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+      options:{
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+],
+};
